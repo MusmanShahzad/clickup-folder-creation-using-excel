3887191a4af0b28eecf6a4d3e3c15f78ca2384f9
usman sahhzad <usmanshahzad641@gmail.com>
2023-11-01 00:10:20 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import axios from "axios";
+// const axios = require("axios").default;
+const app = express();
+
+app.use(express.json());
+
+app.post("/create-folders", async (req, res) => {
+  const { token, folderNames, spaceId } = req.body;
+
+  if (!token || !folderNames) {
+    return res.status(400).send("Missing token or folder names");
+  }
+
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    for (const folderName of folderNames) {
+      // Create a folder
+      let folderResponse = await axios.post(
+        `https://api.clickup.com/api/v2/space/${spaceId}/folder`,
+        {
+          name: folderName,
+        },
+        { headers }
+      );
+
+      let folderId = folderResponse.data.id;
+
+      // Create a list inside the folder
+      await axios.post(
+        `https://api.clickup.com/api/v2/folder/${folderId}/list`,
+        {
+          name: "Legacy",
+        },
+        { headers }
+      );
+    }
+
+    res.send("Folders and lists created successfully.");
+  } catch (error) {
+    console.error("Error creating folders/lists:", error);
+    res.status(500).send("Error creating folders and lists");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
